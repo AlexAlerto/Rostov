8b41ca22145ffa8a457572bbf08ebb50aaa6b07d
alerto <alexbut006@gmail.com>
2025-04-05 17:06:41 +0300

    Initial commit

---
+++ src/main/java/ru/alerto/rostov/RostovApplication.java
@@ -0,0 +1,13 @@
+package ru.alerto.rostov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RostovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alerto/rostov/RostovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alerto.rostov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
